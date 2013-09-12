aa984345cfe81d8d9a11231213a7f743cf485200
vinisauter <vinicius.sauter@hotmail.com>
2013-09-12 13:18:02 -0300

    Init Login

--- index.php
+++ index.php
@@ -1,22 +1,53 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="chrome=1">
-    <title>Projeto Final</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <title>Projeto Final</title>
 
-    <link rel="stylesheet" href="stylesheets/styles.css">
-    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
-    <script src="javascripts/scale.fix.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-  </head>
+        <link rel="stylesheet" href="stylesheets/styles.css">
+        <link rel="stylesheet" href="stylesheets/pygment_trac.css">
+        <script src="javascripts/scale.fix.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
     <body>
-        <p>blabla bla</p>
+        <div class="wrapper">
+            <header>
+                <h1 class="header">Login</h1>
+
+<form method="post" action="index.html" class="login">
+    <p>
+      <label for="login">Email:</label>
+      <input type="text" name="login" id="login" placeholder="exemplo@email.com">
+    </p>
+
+    <p>
+      <label for="password">Senha:</label>
+      <input type="password" name="password" id="password" placeholder="**********">
+    </p>
+
+    <p class="login-submit">
+      <button type="submit" class="login-button">Login</button>
+    </p>
+
+    <p class="forgot-password"><a href="index.html">Esqueceu a sua senha?</a></p>
+ </form>
+                
+            </header>
+            
+            <section>
+                <h3>
+                    <a name="welcome-to-github-pages" class="anchor" href="#welcome-to-github-pages"><span class="octicon octicon-link"></span></a>Welcome to GitHub Pages.</h3>
+
+                <p>This automatic page generator is the easiest way to create beautiful pages for all of your projects. Author your page content here using GitHub Flavored Markdown, select a template crafted by a designer, and publish. After your page is generated, you can check out the new branch:</p>
+
+            </section>
+        </div>
+        <!--[if !IE]><script>fixScale(document);</script><![endif]-->
         <?php
-        // put your code here
         ?>
     </body>
 </html>
